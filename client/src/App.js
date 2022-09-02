3a6d81e81dd75b004c056f10cbd7e1608fbbd0d3
achapur <achapur@palaceresorts.com>
2022-09-01 20:42:08 -0500

    profile reducer half

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import { loadUser } from "./actions/auth";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 //Redux
 import { Provider } from "react-redux";
@@ -32,6 +34,10 @@ const App = () => {
               <Route path="/" element={<Landing />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
+              <Route
+                path="dashboard"
+                element={<PrivateRoute component={Dashboard} />}
+              />
             </Routes>
           </section>
         </Router>
