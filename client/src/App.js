e5057bf06bfd52cba883976fcd0a4a63c213e39d
achapur <achapur@palaceresorts.com>
2022-09-09 17:05:38 -0500

    education done

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 //Redux
@@ -52,6 +53,10 @@ const App = () => {
               <Route
                 path="/add-experience"
                 element={<PrivateRoute component={AddExperience} />}
+              />
+              <Route
+                path="/add-education"
+                element={<PrivateRoute component={AddEducation} />}
               />
             </Routes>
           </section>
